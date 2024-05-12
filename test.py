92f8e461fbb4a283e006ea82dde3e8e7d2924e13
Kevin Zehner <kevzehner@gmail.com>
2024-05-12 08:36:30 +0100

    creates DB

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("Hello World! ")
+import pandas as pd
+import sqlite3
+
+try:
+  
+    csv_file_path = "data.csv"
+
+    df = pd.read_csv(csv_file_path)
+
+    conn = sqlite3.connect("wheelbearings.db")
+
+    df.to_sql("wheelbearing", conn, if_exists="replace", index=False)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+
+    conn.close()
