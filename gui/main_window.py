eb3ab9dbe7c3685c0e74ebb7a315ca4bdecb1845
Kevin Zehner <kevzehner@gmail.com>
2024-07-08 13:08:40 +0100

    adds summary for mai_window.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,8 +1,14 @@
+"""
+This module defines the MainWindow class, which is the main GUI window of the application.
+The MainWindow class inherits from QMainWindow and integrates UI components and logic by
+inheriting from UiComponents and UiLogic classes. The UI layout is loaded from a .ui file,
+and the setup methods for components and logic are called during initialization.
+"""
+
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 from gui.ui_components import UiComponents
 from gui.ui_logic import UiLogic
-
 
 class MainWindow(QMainWindow, UiComponents, UiLogic):
     def __init__(self):
