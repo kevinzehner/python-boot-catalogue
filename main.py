f3e1d933ed09096e45f8c92fc1046d129829842b
Kevin Zehner <kevzehner@gmail.com>
2024-08-02 09:04:47 +0100

    streamlines db operations

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from PyQt5.QtWidgets import QApplication
 from gui import MainWindow  # Ensure you're importing the correct MainWindow class
 
 if __name__ == "__main__":
+    # Create indexes (if they don't exist)
+    from create_indexes import create_indexes
+
+    create_indexes("boots.db")
+
+    # Start the application
     app = QApplication(sys.argv)
     window = MainWindow()
-
-    # Apply the stylesheet
-    with open("style.qss", "r") as style_file:
-        style_content = style_file.read()
-        print(style_content)  # Debug statement to print the content of the stylesheet
-        app.setStyleSheet(style_content)
-
     window.show()
     sys.exit(app.exec_())
